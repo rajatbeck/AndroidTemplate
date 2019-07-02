092f08728d8d154764974e323eb804a615378dcf
rajatbeck <rajatmarcelb@gmail.com>
2019-07-03 00:17:42 +0530

    [rajatbeck/AndroidTemplate#1] added initial dependencies

---
+++ app/src/main/java/com/learn/androidtemplate/network/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.learn.androidtemplate.network
+
+import com.learn.androidtemplate.Api
+import com.learn.androidtemplate.BuildConfig
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule{
+
+
+    @Provides
+    @Singleton
+    fun providesOkHttp():OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor()).build()
+
+    @Provides
+    fun providesMoshi():Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory::class).build()
+
+    @Provides
+    fun providesRetrofit(okHttpClient: OkHttpClient)=
+        Retrofit.Builder().client(okHttpClient).
+            addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL).build()
+
+
+    @Provides
+    fun providesApi(retrofit: Retrofit) = retrofit.create(Api::class.java)
+
+
+
+
+
+}
