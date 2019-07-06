45f3a1af8fa7d082f29961721c321f3e956c2f89
Dharmendra Pratap Singh <dharmendrascpm@gmail.com>
2019-07-06 09:54:43 +0100

    Revert "[rajatbeck/AndroidTemplate#3] Added API keys, API definition and RxJava"

--- app/src/main/java/com/learn/androidtemplate/Api.kt
+++ app/src/main/java/com/learn/androidtemplate/Api.kt
@@ -1,11 +1,5 @@
 package com.learn.androidtemplate
 
-import com.learn.androidtemplate.data.ImageResponse
-import io.reactivex.Single
-import retrofit2.http.GET
-import retrofit2.http.Query
+interface Api{
 
-interface Api {
-    @GET
-    fun getImages(@Query("key") apiKey: String = BuildConfig.API_KEY, @Query("q") query: String): Single<ImageResponse>
 }
--- app/src/main/java/com/learn/androidtemplate/network/NetworkModule.kt
+++ app/src/main/java/com/learn/androidtemplate/network/NetworkModule.kt
@@ -9,32 +9,33 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
-class NetworkModule {
+class NetworkModule{
+
 
     @Provides
     @Singleton
-    fun providesOkHttp(): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor())
-        .build()
+    fun providesOkHttp():OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor()).build()
 
     @Provides
-    fun providesMoshi(): Moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory::class)
-        .build()
+    fun providesMoshi():Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory::class).build()
 
     @Provides
-    fun providesRetrofit(okHttpClient: OkHttpClient) = Retrofit.Builder()
-        .client(okHttpClient)
-        .addConverterFactory(MoshiConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .baseUrl(BuildConfig.BASE_URL).build()
+    fun providesRetrofit(okHttpClient: OkHttpClient)=
+        Retrofit.Builder().client(okHttpClient).
+            addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL).build()
+
 
     @Provides
     fun providesApi(retrofit: Retrofit) = retrofit.create(Api::class.java)
+
+
+
+
 
 }
